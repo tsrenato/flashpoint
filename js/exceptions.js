7304a45f44994d2df2e1f8ae5ee01965eb450b6a
tsrenato <renatootorress@gmail.com>
2022-02-17 17:07:09 -0300

    style: Adds switching flags in language options.

--- js/exceptions.js
+++ js/exceptions.js
@@ -4,12 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let theme0 = document.getElementById('nes');
     let theme1 = document.getElementById('soft');
+    let langBtn = document.getElementById('lang-btn');
 
     [theme0, theme1].forEach((element, index) => {
         element.addEventListener('click', e => {
             e.preventDefault();
             switchToTheme(e.target.id);
         });
+    });
+
+    langBtn.addEventListener('click', e => {
+
+        let lang = localStorage.getItem('lang');
+
+        if (e.target.title == "Protuguês Brasileiro") {
+            e.target.src = "../icons/usa.png";
+            e.target.title = "English USA";
+        } else {
+            e.target.src = "../icons/brasil.png";
+            e.target.title = "Protuguês Brasileiro";
+        }
+
+
     });
 
 });
